c42deacf1ca4ea2aed9b0e0a8605292e4a285e26
Christopher John Jackson <hello@cj-jackson.com>
2021-12-29 20:51:02 +0000

    Init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Shorts
+
+A simply engineered URL shortener that can be manage by terminal.
+No databases or caches just files and that it.
+
+# Usage
+
+```
+# Let's run short in the background
+$ shorts -a :8080 -u /home/user/shorts &
+
+# Let's go to that directory
+$ cd /home/user/shorts
+
+# Add a url, must have .txt extension or it won't work.
+$ echo "https://www.example.com" > example.txt
+
+# Let's test it with curl.
+$ curl -i http://127.0.0.1:8080/example
+HTTP/1.1 308 Permanent Redirect
+Location: https://www.example.com
+Date: Wed, 29 Dec 2021 20:46:43 GMT
+Content-Length: 0
+
+# Let's remove it
+rm example.txt
+
+$ curl -i http://127.0.0.1:8080/example
+HTTP/1.1 404 Not Found
+Date: Wed, 29 Dec 2021 20:48:31 GMT
+Content-Length: 10
+Content-Type: text/plain; charset=utf-8
+
+Not Found
+```
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func main() {
+	var addr, urlLocation string
+
+	{
+		fS := flag.NewFlagSet("shorts", flag.ContinueOnError)
+		fS.StringVar(&addr, "a", ":8080", "Listening Address")
+		fS.StringVar(&urlLocation, "u", "", "Locations of URLs")
+		urlLocation = strings.TrimSpace(urlLocation)
+		if err := fS.Parse(os.Args[1:]); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			fS.PrintDefaults()
+		} else if urlLocation == "" {
+			home, err := os.UserHomeDir()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Must have URL location.")
+				fS.PrintDefaults()
+			}
+			urlLocation = home + "/shorts"
+		}
+		urlLocation = strings.TrimRight(urlLocation, "/")
+	}
+
+	log.Fatal(http.ListenAndServe(addr, http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		serve(writer, request, urlLocation)
+	})))
+}
+
+func serve(writer http.ResponseWriter, request *http.Request, urlLocation string) {
+	fileLocation := urlLocation + "/" + strings.Trim(request.URL.Path, "/") + ".txt"
+	file, err := os.Open(fileLocation)
+	if os.IsNotExist(err) {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(writer, "Not Found")
+		return
+	}
+	defer file.Close()
+
+	if request.Method != http.MethodGet {
+		writer.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(writer, "Not Allowed")
+		return
+	}
+
+	b, err := io.ReadAll(file)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(writer, "Could not read file")
+		return
+	}
+
+	parsedUrl, err := url.Parse(strings.TrimSpace(string(b)))
+	if err != nil || parsedUrl.Scheme == "" {
+		writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(writer, "Could not parse URL correctly")
+		return
+	}
+
+	writer.Header().Set("location", parsedUrl.String())
+	writer.WriteHeader(http.StatusPermanentRedirect)
+}
